d415915790dbc443ec774947037faf443d2e49f1
ExploitEngineer <revilwhoami0098@gmail.com>
2025-09-27 00:34:05 +0500

    feat(validation): add zod validation

---
+++ src/lib/validations.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const userSignUpSchema = z.object({
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  email: z.string().email({ message: "Invalid email address." }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters." }),
+});
+
+export type UserSignUpValues = z.infer<typeof userSignUpSchema>;
+
+export const userSignInSchema = userSignUpSchema.pick({
+  email: true,
+  password: true,
+});
+
+export type UserSignInValues = z.infer<typeof userSignInSchema>;
+
+export const userForgotPasswordSchema = userSignUpSchema.pick({
+  email: true,
+});
+
+export type UserForgotPasswordValues = z.infer<typeof userForgotPasswordSchema>;
